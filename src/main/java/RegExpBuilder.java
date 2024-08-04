c6fc4b1e1f4fb1e899b726d637d1ebaf57ddf87a
KhalezAlex <klozevitz@yandex.ru>
2024-08-04 19:44:49 +0300

    feature(RegExpBuilder): build

---
+++ src/main/java/RegExpBuilder.java
@@ -0,0 +1,11 @@
+public class RegExpBuilder {
+    private final StringBuilder regexp = new StringBuilder();
+
+    /**
+     * builds regular expression
+     */
+
+    public String build() {
+        return regexp.toString();
+    }
+}
